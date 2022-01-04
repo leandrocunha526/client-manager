84174859feb230b24e723d9853554b18d06ed272
leandrocunha526 <leandrocunha016@gmail.com>
2022-01-04 07:22:26 -0300

    Update clients/models.py

--- clients/models.py
+++ clients/models.py
@@ -16,34 +16,10 @@ class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     age = models.IntegerField()
-    salary = models.DecimalField(max_digits=5, decimal_places=2)
+    salary = models.DecimalField(max_digits=5, decimal_places=3)
     biography = models.TextField()
     photo = models.ImageField(upload_to='client_photos', null=True, blank=True)
     document = models.OneToOneField(Document, null=True, blank=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
-
-
-class Product(models.Model):
-    class Meta:
-        verbose_name_plural = "Produtos"
-    description = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
-
-    def __str__(self):
-        return self.description
-
-
-class Sale(models.Model):
-    class Meta:
-        verbose_name_plural = "Vendas"
-    number = models.CharField(max_length=7)
-    value = models.DecimalField(max_digits=5, decimal_places=2)
-    discount = models.DecimalField(max_digits=5, decimal_places=2)
-    taxes = models.DecimalField(max_digits=5, decimal_places=2)
-    person = models.ForeignKey(Person, null=True, blank=True, on_delete=models.PROTECT)
-    products = models.ManyToManyField(Product, blank=True)
-
-    def __str__(self):
-        return self.number
