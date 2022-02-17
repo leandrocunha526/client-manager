bab007e916025047a51935f06733e95cd509f746
leandrocunha526 <leandrocunha016@gmail.com>
2022-02-17 16:58:49 -0300

    Add register and update product

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import products_list, product_delete
+from .views import product_list, product_delete, product_new, product_update
 
 
 urlpatterns = [
-    path('list/', products_list, name="product_list"),
-    path('delete/<int:id>', product_delete, name="product_delete")
+    path('list/', product_list, name="product_list"),
+    path('delete/<int:id>', product_delete, name="product_delete"),
+    path('new/', product_new, name="product_new"),
+    path('update/<int:id>', product_update, name="product_update"),
 ]
