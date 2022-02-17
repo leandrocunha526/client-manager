8126c5e973afef55b425c7f3c8738e0f54f843ab
leandrocunha526 <leandrocunha016@gmail.com>
2022-02-17 16:58:49 -0300

    Add register and update product

--- product/views.py
+++ product/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Product
+from .forms import ProductForm
 
 
 @login_required
-def products_list(request):
+def product_list(request):
     products = Product.objects.all().order_by('id')
     return render(request, 'product.html', {'product': products})
 
@@ -17,3 +18,24 @@ def product_delete(request, id):
         product.delete()
         return redirect('product_list')
     return render(request, 'product_delete_confirm.html', {'product': product})
+
+
+@login_required
+def product_new(request):
+    form = ProductForm(request.POST or None, request.FILES or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('product_list')
+    return render(request, 'product_form.html', {'form': form})
+
+
+@login_required
+def product_update(request, id):
+    product = get_object_or_404(Product, pk=id)
+    form = ProductForm(request.POST or None, request.FILES or None, instance=product)
+
+    if form.is_valid():
+        form.save()
+        return redirect('product_list')
+    return render(request, 'product_form.html', {'form': form})
