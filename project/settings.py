f305607f2672e84b6f5533345bdee89750a71f55
leandrocunha526 <leandrocunha016@gmail.com>
2022-02-18 18:14:47 -0300

    Change allowed hosts

--- project/settings.py
+++ project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ["https://clientsmanager.herokuapp.com/", "localhost"]
+ALLOWED_HOSTS = ["clientsmanager.herokuapp.com/", "localhost"]
 
 
 # Application definition
