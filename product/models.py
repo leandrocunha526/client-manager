3236e2d3dd992a2920e94db0a324caeb2110e0b2
leandrocunha526 <leandrocunha016@gmail.com>
2022-02-20 15:11:57 -0300

    Alter field price on product

--- product/models.py
+++ product/models.py
@@ -12,7 +12,7 @@ class Category(models.Model):
 
 class Product(models.Model):
     description = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
     category = models.ForeignKey(Category,  null=True, blank=True, on_delete=models.PROTECT)
 
     def __str__(self):
