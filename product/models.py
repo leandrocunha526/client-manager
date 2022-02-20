107fdf2a64908c75538ffda2577d54df5d6dd7e4
leandrocunha526 <leandrocunha016@gmail.com>
2022-02-20 00:01:52 -0300

    Add categories in Django Admin
    Import migration generated by Django to category in Django Admin

--- product/models.py
+++ product/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 
 class Category(models.Model):
+    class Meta:
+        verbose_name_plural = "Categorias"
     name = models.CharField(max_length=100)
 
     def __str__(self):
