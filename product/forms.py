75acddfda1586300ee71097e11171d88b3c0c9bb
leandrocunha526 <leandrocunha016@gmail.com>
2022-02-23 11:39:56 -0300

    Add forms labels in Portuguese and change column name in table

--- product/forms.py
+++ product/forms.py
@@ -6,9 +6,18 @@ class ProductForm(ModelForm):
     class Meta:
         model = Product
         fields = ['name', 'description', 'price', 'category']
+        labels = {
+            'name': ("Nome"),
+            'description': ("Descrição"),
+            'price': ("Preço"),
+            'category': ("Categoria")
+        }
 
 
 class CategoryForm(ModelForm):
     class Meta:
         model = Category
         fields = ['name']
+        labels = {
+            'name': ("Nome"),
+        }
